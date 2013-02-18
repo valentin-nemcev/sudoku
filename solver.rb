3f0fde92b8b0555c2d6ec44ef6ae1ff87f100cd1
Валентин Немцев <mail@valentin-nemcev.com>
2013-02-18 16:36:15 +0400

    Solving full solution in 9 sec

--- solver.rb
+++ solver.rb
@@ -5,10 +5,11 @@ require 'set'
 
 
 problems = [
-  # '935748621876231594124695783512469378643872915789153462267514839491386257358927146',
-    '..5..8..187623159.......78....4.....64....9......53..2.6.........138..5735892714.',
+   # '935748621876231594124695783512469378643872915789153462267514839491386257358927146',
+   # '9357486218762315.412469578.512469378643872915789153462267514839491386257358927146',
+    # '..5..8..187623159.......78....4.....64....9......53..2.6.........138..5735892714.',
   # '..5..8..18......9.......78....4.....64....9......53..2.6.........138..5....9.714.',
-  # '..........72.6.1....51...82.8...13..4.........37.9..1.....238..5.4..9.........79.',
+  '..........72.6.1....51...82.8...13..4.........37.9..1.....238..5.4..9.........79.',
   # '...658.....4......12............96.7...3..5....2.8...3..19..8..3.6.....4....473..',
   # '.2.3.......6..8.9.83.5........2...8.7.9..5........6..4.......1...1...4.22..7..8.9',
   # '.5..9....1.....6.....3.8.....8.4...9514.......3....2..........4.8...6..77..15..6.',
@@ -17,7 +18,7 @@ problems = [
 ]
 
 solutions = [
-  '935748621876231594124695783512469378643872915789153462267514839491386257358927146',
+  # '935748621876231594124695783512469378643872915789153462267514839491386257358927146',
   '143258679872964153695137482986541327451372968237896514719623845564789231328415796',
   '937658241864291735125734986583419627649372518712586493471963852396825174258147369',
   '924361758156478293837592641613247985749185326582936174498623517371859462265714839',
@@ -29,132 +30,104 @@ solutions = [
 
 
 class Problem
-  attr_reader :cells, :boxes, :cols, :rows, :blank_indices
+  attr_reader :cells
   def initialize(str_def)
-    @cells = str_def.chars.map do |char|
-      char.match(/\d/) ? char.to_i : nil
+    @cells = (0...9*9).map do |i|
+      (1..9).to_set
     end
-
-    @is_correct = check
-
-    sort_blank_indices
-    advance_blank_index
+    str_def.chars.each_with_index do |char, i|
+      char.match(/\d/) and fill_cell(i, char.to_i)
+    end
   end
 
   def initialize_copy(other)
-    @cells = other.cells.dup
-    @blank_indices = other.blank_indices.dup
-    @boxes = other.boxes.map(&:dup)
-    @cols  = other.cols.map(&:dup)
-    @rows  = other.rows.map(&:dup)
+    @cells = other.cells.map(&:dup)
   end
 
   def to_s
-    @cells.map{ |d| d.nil? ? '.' : d }.join
+    @cells.map do |ds|
+      case ds.length
+      when 0 then 'x'
+      when 1 then  ds.first
+      else '.'
+      end
+    end.join
   end
 
   def solved?
-    @blank_index.nil?
+    @cells.all?{ |ds| ds.length == 1 }
   end
 
   def correct?
-    @is_correct
+    @cells.all?{ |ds| ds.length > 0 }
   end
 
-  def sort_blank_indices
-    cell_constraints = []
-    @blank_indices = []
-    @cells.each_with_index do |el, i|
-      next unless el.nil?
-      col, row, box = sets(i)
-      cell_constraints << [col.length + row.length + box.length, i]
-      @blank_indices << i
+  def fill_cell(cell_i, digit)
+    related_indices(cell_i) do |i|
+      @cells[i].delete(digit)
     end
-    @blank_indices = cell_constraints.sort.map{ |c| c[1] }
-    # ap cell_constraints.sort
+    @cells[cell_i].replace([digit])
   end
 
-  def advance_blank_index
-    # @blank_index = @cells.index(nil)
-    @blank_index = @blank_indices.shift
-  end
-
-  SET_MAP = (0...9*9).map do |i|
-    col_n = i % 9
-    row_n = i / 9
-    box_n = row_n / 3 * 3 + col_n / 3
-    [col_n, row_n, box_n].freeze
-  end.freeze
-
-  def sets(i)
-    col_n, row_n, box_n = SET_MAP[i]
-
-    [@cols[col_n] ||= Set.new, @rows[row_n] ||= Set.new, @boxes[box_n] ||= Set.new]
-  end
-
-  def check
-    @boxes = []
-    @cols  = []
-    @rows  = []
-    @cells.each_with_index do |el, i|
-      next if el.nil?
-      col, row, box = sets(i)
-      return false unless col.add?(el) &&
-        row.add?(el) &&
-        box.add?(el)
-    end
-    true
-  end
-
-  def fill_blanks
-    @cells.each_with_index do |el, i|
-      next unless el.nil?
-      col, row, box = sets(i)
-      if col.length == 8 && row.length == 8 && box.length == 8
-        @cells[i] = ((1..9).to_set - col).first
-        @blank_indices.delete(i)
-        puts "filled #{i}"
+  def related_indices(cell_i)
+    col_n = cell_i % 9
+    row_n = cell_i / 9
+    box_r = row_n / 3
+    box_c = col_n / 3
+    9.times{ |i| yield i*9 + col_n }
+    9.times{ |i| yield row_n*9 + i }
+    3.times do |r|
+      3.times do |c|
+        yield (box_r*3 + r)*9 + (box_c*3 + c)
       end
     end
   end
 
-  def fill_blank(digit)
-    @cells[@blank_index] = digit
-    col, row, box = sets(@blank_index)
-    # @is_correct = col.add?(digit) &&
-    #   row.add?(digit) &&
-    #   box.add?(digit)
-    col << digit
-    row << digit
-    box << digit
-    # @is_correct = check
-    # fill_blanks if @is_correct
-    # sort_blank_indices if @is_correct
-    advance_blank_index
+  def find_next_cell
+    @cells.each_with_index.inject(nil) do |minidx, (ds,i)|
+      if ds.length <= 1
+        minidx
+      else
+        minidx.nil? || (ds.length < @cells[minidx].length) ? i : minidx
+      end
+    end
+    # @cells.index{ |ds| ds.length > 1 }
   end
-
-  DIGITS = (1..9).to_set.freeze
 
   def solve
-    return nil unless correct?
-    return self if solved?
-    col, row, box = sets(@blank_index)
-    (DIGITS - (col | row | box)).each do |d|
-    # (1..9).each do |d|
+    # return self if solved? || !correct?
+    cell_i = find_next_cell
+    # ap cell_i
+    return self if cell_i.nil?
+    @cells[cell_i].each do |d|
+      puts "#{' '*cell_i}#{d}\n#{self}"
       candidate = self.dup
-      candidate.fill_blank(d)
-      possible_solution = candidate.solve
-      return possible_solution unless possible_solution.nil?
+      candidate.fill_cell(cell_i, d)
+      if candidate.correct?
+        next unless self.recheck
+        solution = candidate.solve 
+        return solution if solution.solved?
+      end
     end
-    return nil
+    self
   end
+
+  def recheck
+    rows = @cells.map{ |ds| ds.length == 1 ? ds.first : nil}.each_slice(9).to_a
+    cols = rows.transpose
+    blocks = rows.each_slice(3).flat_map do |rb|
+      rb.transpose.each_slice(3).map(&:flatten)
+    end
+    # ap blocks
+    (rows + cols + blocks).map(&:compact).all? { |r| r.uniq.length == r.length }
+  end
+
 end
 
 
-(problems).each do |problem|
+(problems).zip(solutions).first(1).each do |problem, solution|
   puts problem
-  solution = Problem.new(problem).solve
-  puts solution ? solution.to_s : '---'
-  ap solution.check if solution
-  puts
+  puts s = Problem.new(problem).solve
+  ap s.recheck
+  puts solution
 end
